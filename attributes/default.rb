efebae8e71372eba59de4edfdbfc310e651bcac1
Eric Saxby <eric@wanelo.com>
2015-10-22 13:33:48 -0700

    0.2.0 - install pg_repack 1.3.2 by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['pg_repack']['version'] = '1.2.1'
+default['pg_repack']['version'] = '1.3.2'
 default['pg_repack']['mirror'] = 'https://github.com/reorg/pg_repack/archive'
-default['pg_repack']['checksum'] = '2ebff3c81e42e4459561ee1647cd4779c20af464ff5bf4eed585ab26c4208c86'
+default['pg_repack']['checksum'] = 'f91aac545e18ca8c3c5864b0c2f9e4fe85e0fd3e140800b8eeeca325a5f3c0e0'
